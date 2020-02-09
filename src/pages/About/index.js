63dfda9634b53663a1ca2d2082d2ab53534855a8
Jorge Hecherat <hecherat@gmail.com>
2020-02-09 18:11:21 -0300

    [configuration] - Routes configured with react-router-dom

---
+++ src/pages/About/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div>Hi, I'm at About!</div>;
+};
+
+export default About;
---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+import Home from "../pages/Home";
+import About from "../pages/About";
+
+const Routes = ({ ...props }) => {
+  const DefaultRoute = ({ component, ...rest }) => (
+    <>
+      <Header />
+      <main className="main-content">
+        <Route {...rest} component={component} />
+      </main>
+      <Footer />
+    </>
+  );
+
+  return (
+    <Switch {...props}>
+      <DefaultRoute path="/" exact component={Home} />
+      <DefaultRoute path="/sobre" exact component={About} />
+    </Switch>
+  );
+};
+
+export default Routes;
